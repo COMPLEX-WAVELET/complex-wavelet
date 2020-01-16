0332e8014dcbb23f76f7cb0526a74566af0ddea3
rodinasophie <rodinasophie@gmail.com>
2020-01-16 20:14:41 +0100

    remove log

--- dtcwt_scattering.py
+++ dtcwt_scattering.py
@@ -1,10 +1,12 @@
 import dtcwt
 import numpy as np
 
+
 class DtcwtScattering2D:
     """
     The constructor of a dtwct scattering transform. 
     """
+
     def __init__(self):
         self._transform2D = dtcwt.Transform2d()
 
@@ -12,26 +14,27 @@ class DtcwtScattering2D:
     Applies the scattering network transform to an image
     Returns a set of scattering coefficients.
     """
+
     def _transform(self, image, m):
         n = len(image)
-        J = int(np.log(n/4)/np.log(2))
-        
+        J = int(np.log(n / 4) / np.log(2))
+
         scat_coefficients = []
         image_t = self._transform2D.forward(image, nlevels=J + 1)
         scat_coefficients.append(abs(image_t.lowpass))
-        
+
         if m != 0:
             for j in range(0, J):
                 highpass = image_t.highpasses[j]
                 for k in range(0, highpass.shape[2]):
-                    im = highpass[:,:,k]
+                    im = highpass[:, :, k]
                     scat_coefficients = scat_coefficients + self._transform(im, m - 1)
         return scat_coefficients
-    
+
     """
     Applies log transform to scattering coefficients and returns final result.
     """
+
     def transform(self, image, m):
         scat_coefficients = self._transform(image, m)
-        return np.log(scat_coefficients)
-
+        return scat_coefficients
